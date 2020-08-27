1706cfe711146a23ec706ba82227ec4541b4b1b5
Ann <ann.suparada@gmail.com>
2020-08-27 14:07:58 -0500

    auth user

--- middlewere/auth.js
+++ middlewere/auth.js
@@ -12,11 +12,12 @@ module.exports = function (req, res, next) {
 
   //verify token
   try {
-    const decoded = jwt.verify(token, config.get("jwtSerect"));
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
 
     req.user = decoded.user;
     next();
   } catch (err) {
+    console.log(err);
     res.status(401).json({ msg: "Token is not valid" });
   }
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middlewere/auth");
 
+const User = require("../../modules/User");
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get("/", (req, res) => res.send("auth route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
