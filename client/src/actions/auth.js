9eead8bd2937925468a2d3bf02cd94ba8f7a48b3
Ann <ann.suparada@gmail.com>
2020-09-09 16:39:04 -0500

    debugging

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,8 +1,35 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+} from "./types";
+import setAuthToken from "../utils/setAuthToken";
 
 const URL = "http://localhost:5000";
+
+//Load user
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get(`${URL}/api/auth`);
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (err) {
+    console.log(err);
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
 
 //Register user
 export const register = ({ name, email, password }) => async (dispatch) => {
