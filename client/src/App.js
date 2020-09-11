30c5533f95cb90d3328e812f8b145a550e877c72
Ann <ann.suparada@gmail.com>
2020-09-11 13:48:35 -0500

    profile action

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routhing/PrivateRoute";
 import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
 //redux
@@ -31,8 +33,9 @@ const App = () => {
           <section className="container">
             <Alert />
             <Switch>
-              <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
           </section>
         </>
